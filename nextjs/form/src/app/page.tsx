3c8f683358107e46131dc520dc0b32f77d515f3c
クルクル <64203649+Qluqlu3@users.noreply.github.com>
2023-02-12 03:41:02 +0900

    Delete nextjs/form directory

--- nextjs/form/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className='flex justify-center items-center p-8'>
-      <h1 className='text-white text-7xl font-bold'>タイトル</h1>
-    </main>
-  );
-}
